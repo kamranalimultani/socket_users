5bd36a91c68a6e90eb99b10f3d583334e7deea7c
Mohd Kamran <md.kamran@arnowa.com>
2025-06-16 18:31:06 +0530

    sdfsdf

--- index.js
+++ index.js
@@ -90,6 +90,6 @@ io.on("connection", (socket) => {
   });
 });
 
-server.listen(3000, () => {
-  console.log("🚀 Server running at http://localhost:3000");
+server.listen(1000, () => {
+  console.log("🚀 Server running at http://localhost:1000");
 });
